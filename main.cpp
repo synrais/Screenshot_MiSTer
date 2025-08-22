682c529f7aa1cc413108f8842fc1f19656de9b79
synrais <78070154+synrais@users.noreply.github.com>
2025-08-22 23:25:05 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ with help from the MiSTer contributors including Grabulosaure
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 #include "lib/imlib2/Imlib2.h"
 
@@ -28,14 +29,25 @@ void mister_scaler_free(mister_scaler *);
 
 int main(int argc, char *argv[])
 {
+	const char *output_dir = "/tmp/screenshots";
     char filename[4096];
-    char scaled_filename[4096+512];
-    strcpy(filename,"MiSTer_screenshot.png");
-    if (argc > 1) 
+    strcpy(filename, "MiSTer_screenshot.png");
+    if (argc > 1)
     {
-        fprintf(stderr,"output name: %s\n", argv[1]);
-	strcpy(filename,argv[1]);
+        fprintf(stderr, "output name: %s\n", argv[1]);
+        const char *name = strrchr(argv[1], '/');
+        if (name)
+            name++;
+        else
+            name = argv[1];
+        strncpy(filename, name, sizeof(filename) - 1);
+        filename[sizeof(filename) - 1] = '\0';
     }
+	
+    mkdir(output_dir, 0777);
+
+    char output_path[4096 + 512];
+    snprintf(output_path, sizeof(output_path), "%s/%s", output_dir, filename);
 
     mister_scaler *ms=mister_scaler_init();
     if (ms==NULL)
@@ -46,25 +58,14 @@ int main(int argc, char *argv[])
     fprintf(stderr,"\nScreenshot code by alanswx\n\n");
     fprintf(stderr,"Version %s\n\n", version + 5);
    
-    unsigned char *outputbuf = (unsigned char*)calloc(ms->width*ms->height*4,1);	
-    mister_scaler_read_32(ms,outputbuf);
-    Imlib_Image im = imlib_create_image_using_data(ms->width,ms->height,(unsigned int *)outputbuf);
+    unsigned char *outputbuf = (unsigned char*)calloc(ms->width * ms->height * 4, 1);
+    mister_scaler_read_32(ms, outputbuf);
+    Imlib_Image im = imlib_create_image_using_data(ms->width, ms->height, (unsigned int *)outputbuf);
     imlib_context_set_image(im);
-    if (!strcmp("-",filename))
-    {
-	fprintf(stderr,"not implemented\n");
-    }
-    else
-	imlib_save_image(filename);
-
-
-    sprintf(scaled_filename,"SCALE_%s",filename);
-    printf("filename:[%s]\n",filename);
-
-
-    Imlib_Image im_scaled=imlib_create_cropped_scaled_image(0,0,ms->width,ms->height,ms->output_width,ms->output_height);
+    Imlib_Image im_scaled = imlib_create_cropped_scaled_image(0, 0, ms->width, ms->height, ms->output_width, ms->output_height);
     imlib_context_set_image(im_scaled);
-    imlib_save_image(scaled_filename);
+    imlib_save_image(output_path);
+    printf("saved screenshot to [%s]\n", output_path);
     //free(outputbuf);
     mister_scaler_free(ms); 
 
