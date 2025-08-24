5b1df6242a4b6e1cfa20d324cb79b1dd9a9dc0a6
synrais <78070154+synrais@users.noreply.github.com>
2025-08-25 02:31:58 +1000

    Update mister_peeper.cpp

--- mister_peeper.cpp
+++ mister_peeper.cpp
@@ -1,9 +1,15 @@
 #include <chrono>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
+// Minimal scaler + shared memory implementation so the tool is standalone
 typedef struct {
     int header;
     int width;
@@ -19,12 +25,145 @@ typedef struct {
 
 #define MISTER_SCALER_BASEADDR   0x20000000
 #define MISTER_SCALER_BUFFERSIZE (2048*3*1024)
+#define fpga_mem(x) (0x20000000 | ((x) & 0x1FFFFFFF))
 
-mister_scaler *mister_scaler_init();
-int mister_scaler_read(mister_scaler *, unsigned char *buffer);
-int mister_scaler_read_32(mister_scaler *ms, unsigned char *buffer);
-int mister_scaler_read_yuv(mister_scaler *ms,int,unsigned char *y,int, unsigned char *U,int, unsigned char *V);
-void mister_scaler_free(mister_scaler *);
+void mister_scaler_free(mister_scaler *ms);
+
+// --- shared memory helpers ---
+static int memfd = -1;
+
+void *shmem_map(uint32_t address, uint32_t size) {
+    if (memfd < 0) {
+        memfd = open("/dev/mem", O_RDWR | O_SYNC | O_CLOEXEC);
+        if (memfd == -1) {
+            std::printf("Error: Unable to open /dev/mem!\n");
+            return 0;
+        }
+    }
+    void *res = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, memfd, address);
+    if (res == MAP_FAILED) {
+        std::printf("Error: Unable to mmap (0x%X, %d)!\n", address, size);
+        return 0;
+    }
+    return res;
+}
+
+int shmem_unmap(void *map, uint32_t size) {
+    if (munmap(map, size) < 0) {
+        std::printf("Error: Unable to unmap(0x%X, %d)!\n", (uint32_t)map, size);
+        return 0;
+    }
+    return 1;
+}
+
+int shmem_put(uint32_t address, uint32_t size, void *buf) {
+    void *shmem = shmem_map(address, size);
+    if (shmem) {
+        std::memcpy(shmem, buf, size);
+        shmem_unmap(shmem, size);
+    }
+    return shmem != 0;
+}
+
+int shmem_get(uint32_t address, uint32_t size, void *buf) {
+    void *shmem = shmem_map(address, size);
+    if (shmem) {
+        std::memcpy(buf, shmem, size);
+        shmem_unmap(shmem, size);
+    }
+    return shmem != 0;
+}
+
+// --- scaler access ---
+mister_scaler *mister_scaler_init() {
+    mister_scaler *ms = (mister_scaler*)std::calloc(1, sizeof(mister_scaler));
+    int pagesize = sysconf(_SC_PAGE_SIZE);
+    if (pagesize == 0) pagesize = 4096;
+    int offset = MISTER_SCALER_BASEADDR;
+    int map_start = offset & ~(pagesize - 1);
+    ms->map_off = offset - map_start;
+    ms->num_bytes = MISTER_SCALER_BUFFERSIZE;
+
+    ms->map = (char*)shmem_map(map_start, ms->num_bytes + ms->map_off);
+    if (!ms->map) {
+        mister_scaler_free(ms);
+        return nullptr;
+    }
+
+    unsigned char *buffer = (unsigned char *)(ms->map + ms->map_off);
+    if (buffer[0] != 1 || buffer[1] != 1) {
+        std::fprintf(stderr, "problem\n");
+        mister_scaler_free(ms);
+        return nullptr;
+    }
+
+    ms->header       = buffer[2]  << 8 | buffer[3];
+    ms->width        = buffer[6]  << 8 | buffer[7];
+    ms->height       = buffer[8]  << 8 | buffer[9];
+    ms->line         = buffer[10] << 8 | buffer[11];
+    ms->output_width = buffer[12] << 8 | buffer[13];
+    ms->output_height= buffer[14] << 8 | buffer[15];
+    return ms;
+}
+
+void mister_scaler_free(mister_scaler *ms) {
+    if (ms) {
+        shmem_unmap(ms->map, ms->num_bytes + ms->map_off);
+        std::free(ms);
+    }
+}
+
+int mister_scaler_read(mister_scaler *ms, unsigned char *gbuf) {
+    unsigned char *buffer = (unsigned char *)(ms->map + ms->map_off);
+    for (int y = 0; y < ms->height; y++) {
+        std::memcpy(&gbuf[y*(ms->width*3)],
+                    &buffer[ms->header + y*ms->line],
+                    ms->width*3);
+    }
+    return 0;
+}
+
+int mister_scaler_read_32(mister_scaler *ms, unsigned char *gbuf) {
+    unsigned char *buffer = (unsigned char *)(ms->map + ms->map_off);
+    unsigned char *pixbuf;
+    unsigned char *outbuf;
+    for (int y = 0; y < ms->height; y++) {
+        pixbuf = &buffer[ms->header + y*ms->line];
+        outbuf = &gbuf[y*(ms->width*4)];
+        for (int x = 0; x < ms->width; x++) {
+            outbuf[2] = *pixbuf++;
+            outbuf[1] = *pixbuf++;
+            outbuf[0] = *pixbuf++;
+            outbuf[3] = 0xFF;
+            outbuf += 4;
+        }
+    }
+    return 0;
+}
+
+int mister_scaler_read_yuv(mister_scaler *ms, int lineY, unsigned char *bufY,
+                           int lineU, unsigned char *bufU,
+                           int lineV, unsigned char *bufV) {
+    unsigned char *buffer = (unsigned char *)(ms->map + ms->map_off);
+    for (int y = 0; y < ms->height; y++) {
+        unsigned char *pixbuf = &buffer[ms->header + y*ms->line];
+        unsigned char *outbufy = &bufY[y*lineY];
+        unsigned char *outbufU = &bufU[y*lineU];
+        unsigned char *outbufV = &bufV[y*lineV];
+        for (int x = 0; x < ms->width; x++) {
+            int R = *pixbuf++;
+            int G = *pixbuf++;
+            int B = *pixbuf++;
+            int Y =  (0.257 * R) + (0.504 * G) + (0.098 * B) + 16;
+            int U = -(0.148 * R) - (0.291 * G) + (0.439 * B) + 128;
+            int V =  (0.439 * R) - (0.368 * G) - (0.071 * B) + 128;
+            *outbufy++ = Y;
+            *outbufU++ = U;
+            *outbufV++ = V;
+        }
+    }
+    return 0;
+}
 
 // FNV-1a 64-bit hash
 static uint64_t fnv1a(const unsigned char *data, size_t len) {
